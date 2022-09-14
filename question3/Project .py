a7e2277289ab6f9dfa991ce8c2a8d5bcc7df7cad
Alan-Feder <113436655+Alan-Feder@users.noreply.github.com>
2022-09-14 21:23:07 +0200

    Update Project .py

--- question3/Project .py
+++ question3/Project .py
@@ -1,7 +1,7 @@
 #Import CSV file containing the data, pandas and matplotlib
 import pandas
 from matplotlib import pyplot as plt
-Project = pandas.read_csv('C:\\Users\\edson\\OneDrive\\Documentos\\Project Generation\\Countries with higher revenue.csv')
+Project = pandas.read_csv('Countries with higher revenue.csv')
 
 plt.scatter(Project.CountryRegionCode, Project.Revenue)
 
