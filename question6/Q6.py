48adaf387930e99f9f376c41fbe7b3d0e36f472b
alishaz1 <113436739+alishaz1@users.noreply.github.com>
2022-09-14 21:50:00 +0100

    Update Q6.py

--- question6/Q6.py
+++ question6/Q6.py
@@ -6,11 +6,11 @@ print(exampletable.head())
 #create a graph
 from matplotlib import pyplot as plt
 plt.plot(exampletable.annualrevenue, exampletable.numberofemployees, exampletable.storesize_ft)
-plt.scatter(exampletable.annualrevenue.x, exampletable.numberofemployees.x, exampletable.storesize_ft.x. figures.y)
+plt.scatter(exampletable.annualrevenue.x, exampletable.numberofemployees.x, exampletable.y)
 
 plt.title("The relationship between the size of the stores, number of employees and revenue")
-plt.ylabel('Figure')
-plt.xlabel('numberofemployees')('annualrevenue')('storesize_ft')
+plt.ylabel('storesize_ft')
+plt.xlabel('numberofemployees')('annualrevenue')
 
 plt.legend()
 plt.show()
