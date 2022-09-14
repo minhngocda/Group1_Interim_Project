8c6b4faceaae4da2193d63a2e8eb320f9564a8b7
Shay Martino <59856512+shay-brazil@users.noreply.github.com>
2022-09-14 16:46:20 -0300

    Delete Question2UsingBonus.sql

--- question2/Question2UsingBonus.sql
+++
@@ -1,6 +0,0 @@
-SELECT VacationHours, 
-        Bonus
-FROM HumanResources.Employee
-INNER JOIN Sales.SalesPerson
-ON Sales.SalesPerson.BusinessEntityID = HumanResources.Employee.BusinessEntityID
-ORDER BY Bonus DESC
