c29f88f9969b2290fee7ecc1ec5e248e60e67fc2
alishaz1 <113436739+alishaz1@users.noreply.github.com>
2022-09-14 16:50:31 +0100

    Add files via upload
    draft

---
+++ question6/Q6.py
@@ -0,0 +1,16 @@
+#import data from excel onto python
+import pandas
+exampletable = pandas.read_csv('C:\\Users\\User\\Documents\\Interim Project\\Question6.csv')
+print(exampletable.head())
+
+#create a graph
+from matplotlib import pyplot as plt
+plt.plot(exampletable.annualrevenue, exampletable.numberofemployees, exampletable.storesize_ft)
+plt.scatter(exampletable.annualrevenue.x, exampletable.numberofemployees.x, exampletable.storesize_ft.x. figures.y)
+
+plt.title("The relationship between the size of the stores, number of employees and revenue")
+plt.ylabel('Figure')
+plt.xlabel('numberofemployees')('annualrevenue')('storesize_ft')
+
+plt.legend()
+plt.show()
