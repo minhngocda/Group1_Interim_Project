ccde29016e18dd3d172b83fc61b4a9dfd4731406
Abdul Soomro <113062074+abtwoy@users.noreply.github.com>
2022-09-15 12:51:50 +0100

    Delete Question4V2.sql

--- question4/Question4V2.sql
+++
@@ -1,5 +0,0 @@
-SELECT hre.OrganizationLevel, pp.PersonType, AVG(hre.SickLeaveHours) AS AverageSickLeaveHours
-FROM HumanResources.Employee AS hre
-INNER JOIN Person.Person AS pp ON pp.BusinessEntityID = hre.BusinessEntityID
-GROUP BY hre.OrganizationLevel, pp.PersonType
-ORDER BY AVG(hre.SickLeaveHours) DESC;
