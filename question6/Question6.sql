fbcb60dc441fa7658e4d04e406c60bb86636f095
Minh Ngoc Nguyen <68969499+minhngocda@users.noreply.github.com>
2022-09-15 11:33:42 +0100

    updated query for question 6

--- question6/Question6.sql
+++ question6/Question6.sql
@@ -1,7 +1,13 @@
-
 SELECT 
-	NumberEmployees,
-	AnnualRevenue,
-	SquareFeet
-FROM Sales.vStoreWithDemographics
-ORDER BY NumberEmployees ASC
+	s.Name,
+	s.BusinessEntityID, 
+	SUM(o.TotalDue) as Revenue,   
+	d.SquareFeet, 
+	d.NumberEmployees
+FROM Sales.SalesOrderHeader AS o
+INNER JOIN Sales.Store AS s
+	ON o.SalesPersonID = s.SalesPersonID
+INNER JOIN Sales.vStoreWithDemographics AS d
+	ON s.BusinessEntityID = d.BusinessEntityID
+GROUP BY o.SalesPersonID, s.BusinessEntityID, s.Name, d.SquareFeet, d.NumberEmployees
+ORDER BY Revenue;
