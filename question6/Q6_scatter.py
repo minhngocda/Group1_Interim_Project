77be740f76ec25dd566176b5cfd0152853b7a0e9
Minh Ngoc Nguyen <68969499+minhngocda@users.noreply.github.com>
2022-09-15 15:31:18 +0100

    Create Scatter plot for 3 variables

---
+++ question6/Q6_scatter.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+tb1 = pd.read_csv('C:\\Users\\Admin\\Desktop\\interim project\\question6.csv')
+print(tb1.info())
+
+
+plt.scatter(tb1.NumberEmployees, tb1.SquareFeet, s = tb1.Revenue/1000000, alpha = 0.4)
+plt.xlabel('Number of Employees')
+plt.ylabel('Store size (square feet)')
+plt.title("Relationship betwwen Store Size, Number of Employees and Revenue")
+plt.show()
