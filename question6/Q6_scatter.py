6e26555afa4bfbeef5efb785b62d9d6514964e10
Minh Ngoc Nguyen <68969499+minhngocda@users.noreply.github.com>
2022-09-15 15:37:44 +0100

    Update Q6_scatter.py

--- question6/Q6_scatter.py
+++ question6/Q6_scatter.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
+#import CSV file as a dataframe tb1
 tb1 = pd.read_csv('C:\\Users\\Admin\\Desktop\\interim project\\question6.csv')
-print(tb1.info())
 
-
+#create scatter plot with x axis is Number of Employees, y axis is Store Size and size of dot represent Revenue
 plt.scatter(tb1.NumberEmployees, tb1.SquareFeet, s = tb1.Revenue/1000000, alpha = 0.4)
+
+# Add label for x, y, and tittle
 plt.xlabel('Number of Employees')
 plt.ylabel('Store size (square feet)')
 plt.title("Relationship between Store Size, Number of Employees and Revenue")
+#Show the chart
 plt.show()
