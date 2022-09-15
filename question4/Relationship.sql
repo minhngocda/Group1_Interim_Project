76f430dba8585d68aae51dfa9ee07b804a2de630
Abdul Soomro <113062074+abtwoy@users.noreply.github.com>
2022-09-15 11:00:34 +0100

    Delete Relationship.sql

--- question4/Relationship.sql
+++
@@ -1,4 +0,0 @@
-SELECT pp.PersonType, hre.JobTitle, hre.SickLeaveHours
-FROM HumanResources.Employee AS hre
-INNER JOIN Person.Person AS pp ON pp.BusinessEntityID = hre.BusinessEntityID
-ORDER BY hre.SickLeaveHours DESC
